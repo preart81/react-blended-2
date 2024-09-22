7a05e25dd5fa261bf841d66b9a81306015df079e
Artur Prepelytsia <preart81@gmail.com>
2024-09-22 10:57:34 +0300

    todos

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App.jsx';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Toaster />
   </React.StrictMode>,
 );
